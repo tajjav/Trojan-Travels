43ea5892f65fa1294aeebc72219ae03b62d87252
Tauqeer Javaid <taj.jav@hotmail.com>
2023-09-15 11:43:03 -0600

    deleted student
    student deleted

--- StudentManagementSystem.WEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentManagementSystem.WEB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- StudentManagementSystem.WEB/ViewModels/EditStudentViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudentManagementSystem.WEB.ViewModels
-{
-    public class EditStudentViewModel : CreateStudentViewModel
-    {
-        public int Id { get; set; }
-        public string? ExistingPhoto { get; set; }
-    }
-}
